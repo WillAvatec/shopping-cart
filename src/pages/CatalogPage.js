0530c0e1bc28392ee15b93590f714b2548f6b589
Will <avatec2312@gmail.com>
2023-05-23 00:52:10 -0500

    refactor: Change rest api category option

--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from "react"
 import Header from "../components/ui/Header/Header";
+import Card from "../components/card/Card";
+import "../assets/card.css"
+import "../assets/catalog.css"
+
+const ENDPOINT_CATEGORY = "men's clothing"
 
 function CatalogPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    if(!products.length){
+      fetch(`https://fakestoreapi.com/products/category/${ENDPOINT_CATEGORY}`)
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+    }
+  },[products])
+
   return (
     <div className="home">
       <Header />
+      <div id="cards-container">
+        {products.length
+          ? products.map(product => {
+            return (
+              <Card 
+                image={product.image} 
+                title={product.title}
+                description={product.description}
+                price={product.price} 
+              />
+            )
+          })
+          : null
+        }
+      </div>
     </div>
   );
 }
