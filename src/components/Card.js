bb53a853deccefc4ad19f4abdbb02d9253f51f78
Will <avatec2312@gmail.com>
2023-05-25 04:35:08 -0500

    refact: Move Card.js outside folder and add styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
-import Back from '../../assets/images/logo.png'
+import Back from '../assets/images/logo.png'
+import { useCart } from '../hooks/useCart'
 
-const Card = ({title,price,description,image}) => {
+const Card = ({title,price,image,product}) => {
+    const { addToCart } = useCart()
 
     return (
         <div className="card" >
             <div className="img-container">
-                <img className="front-card-img" alt={`product${title.split(' ',0).join()}`} src={image}/>
+                <img className="front-card-img" alt={`product${title}`} src={image}/>
                 <img className="back-card-img" alt={`product`} src={Back} />
             </div>
             <div className="def-container">
                 <div className="card-title">{title}</div>
                 {/*<div className="card-description">{description}</div>*/ }
                 <div className="card-price">${price}</div>
+            </div>
+            <div className='btn-container'>
+                <button onClick={()=>{addToCart(product)}}>Add to Cart</button>
             </div>
         </div>
     )
