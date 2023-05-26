8f2164731c3eef9e0f516a6e1221746cf06b5815
Will <avatec2312@gmail.com>
2023-05-25 22:05:58 -0500

    recovering from the git fiasco

--- src/components/ui/Header/Head.top.js
+++ src/components/ui/Header/Head.top.js
@@ -1,33 +1,16 @@
 import logo from "../../../assets/images/logo.png"
+import Cart from "../../Cart"
+import { useCart } from "../../../hooks/useCart"
 const Top = () => {
+    const { getPrice } = useCart();
+
     return (
-        <div className='wrapper'>
-            <div className="top-header">
-                <div className="logo">
-                    <img alt="no-logo" src={logo}/>
-                </div>
-                <div className='input-group'>
-                    <div className="categories">
-                        <select defaultValue={'all'} name="categories">
-                            <option value='promociones'>Promociones</option>
-                            <option value='vendidos'>Mas Vendidos</option>
-                            <option value='all'> All Categories</option>
-                        </select>
-                    </div>
-                    <div className="search">
-                        <input type='text' placeholder="What do you need?" />
-                        <button id="search">
-                            <i className='lupa-search'/>
-                        </button>
-                    </div>
-                </div>
-                <div className="header-buttons">
-                    {/* Here is the shopping cart icon */}
-                    <div>
-                        $0.00
-                    </div>
-                </div>
+        <div className="top-header">
+            <div className="logo">
+                <img alt="no-logo" src={logo}/>
             </div>
+            <Cart />
+            ${getPrice()}.00
         </div>
     )
 }
