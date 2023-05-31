5816d07251001a459b781d00aaf7fd68107d2701
Will <avatec2312@gmail.com>
2023-05-30 21:19:21 -0500

    refact: Change from BrowseRouter to HashRouter

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -1,17 +1,17 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { HashRouter,Routes,Route } from "react-router-dom";
 import { HomePage, CatalogPage, ContactUs, ProductPage } from "./pages/"
 import { CartProvider } from "./context/cart";
 const RouteSwitch = () => {
     return (
         <CartProvider>
-            <BrowserRouter>
+            <HashRouter>
                  <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/shop-catalog" element={<CatalogPage/>}/>
                     <Route path='/shop-catalog/:id'element={<ProductPage/>} />
                     <Route path='/contact-us' element={<ContactUs/>} />
                  </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </CartProvider>
     )
 }
