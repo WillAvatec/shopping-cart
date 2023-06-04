d344af617db29910aa25966192097257ac737040
Will <avatec2312@gmail.com>
2023-06-04 02:49:47 -0500

    refact: Show svg icons in header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 import {ReactComponent as ReactLogo} from '../../assets/images/logo.svg';
+import {ReactComponent as CartSvg} from '../../assets/images/cart.svg';
+import {ReactComponent as SearchSvg} from '../../assets/images/search.svg';
+import {ReactComponent as UserSvg} from '../../assets/images/user.svg';
+
 import "../../assets/header.css";
 
 function Header (){
@@ -11,7 +15,7 @@ function Header (){
             </Link>
             </div>
             <nav>
-                <ul>
+                <ul id="links-container">
                     <li><Link to="/our-story">Our History</Link></li>
                     <li><Link to="/shop-catalog">Shop</Link></li>
                     <li><Link to="/contact-us">Contact Us</Link></li>
@@ -19,9 +23,9 @@ function Header (){
             </nav>
             <div className="cart-buttons">
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li id="js-search-icon"> <SearchSvg/> </li>
+                    <li id="js-cart-icon"> <CartSvg/> </li>
+                    <li id="js-user-icon"> <UserSvg/> </li>
                 </ul>
             </div>
         </header>
