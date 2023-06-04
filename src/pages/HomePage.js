b9b527f98f881c9353df84f2a31c4fa696117779
Will <avatec2312@gmail.com>
2023-06-04 02:54:22 -0500

    feat: New Content on HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,23 @@
 import Header from "../components/ui/Header";
+import "../assets/homepage.css";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="home">
       <Header />
+      <div className="home-wrapper">
+        <div className="home-content">
+          <h1>Men's Fashion<span>Boutique</span></h1>
+          <div className="img-content">
+            <div id="image-fade"></div>
+          </div>
+        </div>
+        <div className="season-promotions">
+          <p>New Summer's Collection</p>
+          <Link to="/shop-catalog"><button>Buy Now</button></Link>
+        </div>
+      </div>
     </div>
   );
 }
