a7d6409ef1706e934588be30b56e5b9e088b3fda
Will <avatec2312@gmail.com>
2023-06-05 14:48:50 -0500

    refact: Use router hook to change Url

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,16 @@
 import Header from "../components/ui/Header";
 import "../assets/homepage.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div className="home">
       <Header />
       <div className="home-wrapper">
         <div className="home-content">
+          {/* <p className="clip-text">Style</p> */}
           <h1>Men's Fashion<span>Boutique</span></h1>
           <div className="img-content">
             <div id="image-fade"></div>
@@ -15,7 +18,7 @@ function HomePage() {
         </div>
         <div className="season-promotions">
           <p>New Summer's Collection</p>
-          <Link to="/shop-catalog"><button>Buy Now</button></Link>
+          <button onClick={()=>{navigate("/shop-catalog")}}>Go to Shop</button>
         </div>
       </div>
     </div>
