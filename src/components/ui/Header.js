e0db5575452d5db8d6931edf31dfdfd07aeb5ff4
Will <avatec2312@gmail.com>
2023-06-06 04:08:39 -0500

    feat: New hamburguer button for small devices

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,19 +3,22 @@ import {ReactComponent as ReactLogo} from '../../assets/images/logo.svg';
 import {ReactComponent as CartSvg} from '../../assets/images/cart.svg';
 import {ReactComponent as SearchSvg} from '../../assets/images/search.svg';
 import {ReactComponent as UserSvg} from '../../assets/images/user.svg';
-
+import { ReactComponent as MenuSvg } from "../../assets/images/burger-menu-svgrepo-com.svg";
+import { useState } from "react";
 import "../../assets/header.css";
 
 function Header (){
+    const [toggle, setToggle] = useState(true) ;
     return (
         <header>
             <div id="logo__container">
-            <Link to="/">
-                <ReactLogo />
-            </Link>
-            </div>
+                <Link to="/">
+                    <ReactLogo />
+                </Link>
+            </div>        
+            <MenuSvg  id="mobile-button" onClick={()=>{setToggle(!toggle)}}/>
             <nav>
-                <ul id="links-container">
+                <ul className={toggle ? "active" : ""}  id="links-container">
                     <li><Link to="/our-story">Our Story</Link></li>
                     <li><Link to="/shop-catalog">Shop</Link></li>
                     <li><Link to="/contact-us">Contact Us</Link></li>
