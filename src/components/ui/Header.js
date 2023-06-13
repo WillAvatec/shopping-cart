282e3833e0c67ee63df31aedde107a286c962e7a
Will <avatec2312@gmail.com>
2023-06-13 11:57:06 -0500

    feat: Add new route our-story

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Cart from "../Cart";
 import {ReactComponent as ReactLogo} from '../../assets/images/logo.svg';
-import {ReactComponent as CartSvg} from '../../assets/images/cart.svg';
 import {ReactComponent as SearchSvg} from '../../assets/images/search.svg';
 import {ReactComponent as UserSvg} from '../../assets/images/user.svg';
 import { ReactComponent as MenuSvg } from "../../assets/images/burger-menu-svgrepo-com.svg";
 import { useState } from "react";
 import "../../assets/header.css";
 
-function Header (){
-    const [toggle, setToggle] = useState(true) ;
+function Header () {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <header>
             <div id="logo__container">
@@ -16,9 +17,9 @@ function Header (){
                     <ReactLogo />
                 </Link>
             </div>        
-            <MenuSvg  id="mobile-button" onClick={()=>{setToggle(!toggle)}}/>
+            <MenuSvg  id="mobile-button" onClick={()=>{setToggleMenu(!toggleMenu)}}/>
             <nav>
-                <ul className={toggle ? "active" : ""}  id="links-container">
+                <ul className={toggleMenu ? "active" : ""}  id="links-container">
                     <li><Link to="/our-story">Our Story</Link></li>
                     <li><Link to="/shop-catalog">Shop</Link></li>
                     <li><Link to="/contact-us">Contact Us</Link></li>
@@ -27,7 +28,7 @@ function Header (){
             <div className="cart-buttons">
                 <ul>
                     <li id="js-search-icon"> <SearchSvg/> </li>
-                    <li id="js-cart-icon"> <CartSvg/> </li>
+                    <li id="js-cart-icon"> <Cart/> </li>
                     <li id="js-user-icon"> <UserSvg/> </li>
                 </ul>
             </div>
