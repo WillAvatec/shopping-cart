ed90d50d7d2bb4416fc7b942fb62f6c40009a613
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-03 11:37:19 -0500

    fix: Link in Card Comp. getting over aside

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const Card = ({product}) => {
-    /* const { addToCart } = useCart(); */
-    const { title, price,images,brand} = product;
-    return (
-        <article className="card" >
+const Card = ({ product }) => {
+  /* const { addToCart } = useCart(); */
+  const { title, price, images, brand } = product;
+  return (
+    <article className="card">
+      <div className="img-container">
+        <Link to={`./${title}`}>
+          <img
+            className="front-card-img"
+            alt={`product${title}`}
+            src={images[0]}
+          />
+        </Link>
+      </div>
 
-            <div className="img-container">
-                <img className="front-card-img" alt={`product${title}`} src={images[0]}/>
-                <Link to={`./${title}`} />
-            </div>
+      <div className="def-container">
+        <p className="def-brand">{brand}</p>
 
-            <div className="def-container">
-                <p className='def-brand'>{brand}</p>
+        <Link className="def-title" to={`./${title}`}>
+          {title}
+        </Link>
+        <p className="def-price">USD {price}</p>
+      </div>
 
-                <Link className='def-title' to={`./${title}`}>
-                    {title}
-                </Link>
-                <p className="def-price">USD {price}</p>
-            </div>
-
-            <div>
-                
-            </div>
-
-            {/*This was the previous approach to change url
+      {/*This was the previous approach to change url
                 <div className='btn-container'>
                     <button onClick={()=>{addToCart(product)}}>Add to Cart</button>
                 </div>
             */}
+    </article>
+  );
+};
 
-        </article>
-    )
-}
-
-export default Card
+export default Card;
