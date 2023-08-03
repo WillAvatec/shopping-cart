ceb8b6227b66a409838e7db6ff5d1f696f328664
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-03 11:48:27 -0500

    Add simple placeholder text ContactUs Page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,11 +1,32 @@
 import Header from "../components/ui/Header";
 
-function ContactUs () {
-    return (
-        <div className="home">
-            <Header />
-        </div>
-    )
+function ContactUs() {
+  return (
+    <div className="home">
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginLeft: "3rem",
+          marginRight: "3rem",
+        }}
+      >
+        <h1>Contact Us!</h1>
+        <p style={{ fontSize: "1.3rem", margin: "0 4rem" }}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat.
+          <br />
+          <br /> Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum."
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default ContactUs
+export default ContactUs;
