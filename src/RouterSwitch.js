a2cca37c66c844ad7db2f6c35f88a43f597e5746
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-03 11:35:34 -0500

    feat: New OurStory Page added to router

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -1,20 +1,21 @@
-import { HashRouter,Routes,Route } from "react-router-dom";
-import { HomePage, CatalogPage, ContactUs, ProductPage } from "./pages/"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { HomePage, CatalogPage, ContactUs, ProductPage } from "./pages/";
 import { CartProvider } from "./context/cart";
+import StoryPage from "./pages/StoryPage";
 const RouteSwitch = () => {
-    return (
-        <CartProvider>
-            <HashRouter>
-                 <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/our-story" element={<CatalogPage/>}/>
-                    <Route path="/shop-catalog" element={<CatalogPage/>}/>
-                    <Route path='/shop-catalog/:id'element={<ProductPage/>} />
-                    <Route path='/contact-us' element={<ContactUs/>} />
-                 </Routes>
-            </HashRouter>
-        </CartProvider>
-    )
-}
+  return (
+    <CartProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/our-story" element={<StoryPage />} />
+          <Route path="/shop-catalog" element={<CatalogPage />} />
+          <Route path="/shop-catalog/:id" element={<ProductPage />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </HashRouter>
+    </CartProvider>
+  );
+};
 
-export default RouteSwitch
+export default RouteSwitch;
---
+++ src/pages/StoryPage.js
@@ -0,0 +1,44 @@
+import Header from "../components/ui/Header";
+
+function StoryPage() {
+  return (
+    <div className="home">
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginLeft: "3rem",
+          marginRight: "3rem",
+        }}
+      >
+        <h1>What are we?</h1>
+        <p style={{ fontSize: "1.3rem", margin: "0 4rem" }}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat.
+          <br />
+          <br /> Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum."
+        </p>
+        <h1>What's our goal?</h1>
+        <p style={{ fontSize: "1.3rem", margin: "0 4rem" }}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat.
+          <br />
+          <br /> Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum."
+        </p>
+      </div>
+    </div>
+  );
+}
+export default StoryPage;
